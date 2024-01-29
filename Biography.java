7562390ffcde39d3f5ab331856f4c3cf1095ffb7
Rajeshwari <kapseraji17@gmail.com>
2024-01-29 10:17:03 +0530

    Adding files

---
+++ Biography.java
@@ -0,0 +1,12 @@
+public class Biography {
+    public static void main(String[] args)
+    {
+        String name = "Sam";
+        String nationality = "India";
+        int age = 35;
+        String sport = "Foot ball";
+        System.out.println("My name is " +name + " I am " +age + "Years old");
+        System.out.println("I am from "+nationality+ " My favorite sport is "+sport);
+    }
+    
+}
