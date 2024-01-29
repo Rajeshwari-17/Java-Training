4dc95c826e0e63fa47cbc0f84485db2d05ae01e5
Rajeshwari <kapseraji17@gmail.com>
2024-01-29 13:16:34 +0530

    Adding Files

---
+++ if_else_condition.java
@@ -0,0 +1,15 @@
+public class if_else_condition {
+    public static void main(String[] args)
+  {
+    double a = -0.8;
+    if (a>0)
+    {
+      System.out.println(a + " is a Positive Number!");
+    }
+    else
+    {
+      System.out.println(a + " is a Negative Number!"); 
+    }
+  }
+    
+}
