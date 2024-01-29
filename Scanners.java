82c22839cd9f6fc2bc6329eed5a1bf68b1cb7002
Rajeshwari <kapseraji17@gmail.com>
2024-01-29 13:11:41 +0530

    Adding Files

---
+++ Scanners.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+public class Scanners {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str);
+
+    }
+}
