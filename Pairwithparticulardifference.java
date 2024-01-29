07e8e42984101d20ed5bc3102213ef8dd0d194a2
Rajeshwari <kapseraji17@gmail.com>
2024-01-29 19:58:48 +0530

    Adding files

---
+++ Pairwithparticulardifference.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+class Pairwithparticulardifference {
+	static int maxSumPairWithDifferenceLessThanK(int arr[],int N, int K)
+	{
+		Arrays.sort(arr);
+        int dp[] = new int[N];
+	    dp[0] = 0;
+	    for (int i = 1; i < N; i++)
+		{
+			dp[i] = dp[i-1];
+	         if (arr[i] - arr[i-1] < K)
+			{
+				
+				
+				if (i >= 2)
+					dp[i] = Math.max(dp[i], dp[i-2] + arr[i] +
+													arr[i-1]);
+				else
+					dp[i] = Math.max(dp[i], arr[i] + arr[i-1]);
+			}
+		}
+	
+		
+		return dp[N - 1];
+	}
+
+	
+	public static void main (String[] args) {
+		
+		int arr[] = {3, 5, 10, 15, 17, 12, 9};
+		int N = arr.length;
+		int K = 4;
+		
+		System.out.println ( maxSumPairWithDifferenceLessThanK(
+													arr, N, K));
+		
+	}
+}
