b095cf5626b434779fa96b41fbf99b7540cbd392
Rajeshwari <kapseraji17@gmail.com>
2024-01-30 17:52:58 +0530

    Adding files

---
+++ ListCollection/example1/Book.java
@@ -0,0 +1,60 @@
+public class Book{
+    private String title;
+    private String author;
+    private double rating;
+    private double price;
+
+    public Book(String title,String author,double rating, double price)
+    {
+        this.title = title;
+        this.author = author;
+        this.rating = rating;
+        this.price = price;
+
+    }
+
+    public Book(Book source)
+    {
+        this.title = source.title;
+        this.author = source.author;
+        this.rating = source.rating;
+        this.price = source.price;
+        
+    }
+
+    public String getTitle()
+    {
+        return this.title;
+    }
+    public void setTitle()
+    {
+        this.title = title;
+    }
+    public String getAuthor()
+    {
+        return this.author;
+    }
+    public void setAuthor()
+    {
+        this.author = author;
+    }
+    public double getRating()
+    {
+        return this.rating;
+    }
+    public void setRating()
+    {
+        this.rating = rating;
+    }
+    public double getPrice()
+    {
+        return this.price;
+    }
+    public void setPrice()
+    {
+        this.price = price;
+    }
+    
+
+    
+}
